f2de1b338bffe933e8d87fd208624f43ab636919
Denis Lysikov <dely@simcorp.com>
2022-11-10 17:13:58 +0100

    Initial

---
+++ Tasks/SampleTest.cs
@@ -0,0 +1,13 @@
+﻿using NUnit.Framework;
+
+namespace Tasks;
+
+public class SampleTest
+{
+    [Test]
+    public void TestFiles()
+    {
+        var input = Tools.ReadFromInput("Test.txt");
+        Tools.WriteToOutput("Test2.txt", input);
+    }
+}
---
+++ Tasks/Tools.cs
@@ -0,0 +1,8 @@
+﻿namespace Tasks;
+
+public static class Tools
+{
+    public static string[] ReadFromInput(string fileName) => File.ReadAllLines($@"..\..\..\Input\{fileName}");
+
+    public static void WriteToOutput(string fileName, string[] content) => File.WriteAllLines($@"..\..\..\Output\{fileName}", content);
+}
